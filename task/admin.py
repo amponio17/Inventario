d2025bdc959097a9eca5fb470fc9bb32925ec0ed
amponio17 <tonylvhdp2@gmail.com>
2025-04-29 09:51:48 -0600

    modifique las vistas de administrador

--- task/admin.py
+++ task/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import supliers
-from .models import categories
-from .models import items
-from .models import positions
-from .models import users
+from .models import Provedor
+from .models import categoria
+from .models import productos
+from .models import posicione
+from .models import usuarios
 from .models import stock
 from .models import stock_tracking
 # Register your models here.
@@ -11,12 +11,44 @@ from .models import stock_tracking
 
 
 
-#admin.site.register(supliers),
-admin.site.register(supliers),
-admin.site.register(categories),
-admin.site.register(items),
-admin.site.register(positions),
-admin.site.register(users),
-admin.site.register(stock),
-admin.site.register(stock_tracking),
+#vista de datos de provedor 
+@admin.register(Provedor)
+class Provedor(admin.ModelAdmin):
+    list_display=('suplier_id', 'suplier_name','suplier_Tel','suplier_address','suplier_owner')
 
+
+#vista de datos de productos
+@admin.register(productos)
+class productos(admin.ModelAdmin):
+    list_display=('item_id', 'item_name', 'item_category_id','item_suplier_id')
+
+#vista de datos de usuarios
+@admin.register(usuarios)
+class usuarios(admin.ModelAdmin):
+    list_display=('user_id', 'user_name', 'user_availability')
+
+#vista de datos de stock
+@admin.register(stock)
+class stock(admin.ModelAdmin):
+    list_display=('stock_id', 'stock_item_id', 'stock_position_id', 'stock_itme_amount', 'stock_deseado')
+
+#vista de datos de stock_tracking
+@admin.register(stock_tracking)
+class stock_tracking(admin.ModelAdmin):
+    list_display=('st_id', 'st_id_stock', 'st_id_user', 'st_prev', 'st_act')
+
+#vista de datos de categoria
+@admin.register(categoria)
+class categoria(admin.ModelAdmin):
+    list_display=('category_name',)
+
+#vista de datos de categoria
+@admin.register(posicione)
+class posicione(admin.ModelAdmin):
+    list_display=('position_id','posotion_index','position_aviable',)
+
+#admin.site.register(categoria),
+#admin.site.register(posicione),
+#admin.site.register(usuarios),
+#admin.site.register(stock),
+#dmin.site.register(stock_tracking),
--- task/models.py
+++ task/models.py
@@ -1,60 +1,85 @@
 from django.db import models
 
 #provedores
-class supliers (models.Model):
+class Provedor (models.Model):
     suplier_id = models.AutoField(
-          primary_key=True, 
+          primary_key=True,
+          verbose_name='ID', 
           null=False)
     suplier_name = models.CharField(
          verbose_name='nombre',
            max_length=50,
              null=False)
-    suplier_address = models.CharField(
+    suplier_address = models.CharField(verbose_name='Email',
          max_length=100)
-    suplier_Tel = models.PositiveIntegerField(
+    suplier_Tel = models.IntegerField(verbose_name='Celular',
          null=False)
-    suplier_owner = models.CharField(
+    suplier_owner = models.CharField(verbose_name='Encargado',
          max_length=50,
          null=False)
+    def __str__(self):
+         return self.suplier_name
 
 #categorias
-class categories(models.Model):
-        category_id =models.AutoField(primary_key=True, null=False)
-        category_name =models.CharField(verbose_name='nombre', max_length=50)
+class categoria(models.Model):
+        category_id =models.AutoField(
+            primary_key=True,
+            verbose_name='ID', 
+            null=False),
+        category_name =models.CharField(
+            verbose_name='nombre',
+              max_length=50)
+        def __str__(self):
+             return self.category_name
+
+    
+
 
 #productos
-class items(models.Model):
-    item_id = models.AutoField(primary_key=True, null=False)
+class productos (models.Model):
+    item_id = models.AutoField(primary_key=True, verbose_name='ID', null=False)
     item_name = models.CharField(verbose_name='producto', max_length=50, null=False)
-    item_category_id = models.ForeignKey(categories, on_delete=models.CASCADE,null=False)
-    item_suplier_id = models.ForeignKey(supliers, on_delete=models.CASCADE,null=False)
-
+    item_category_id = models.ForeignKey(categoria, on_delete=models.CASCADE,null=False, verbose_name='categoria')
+    item_suplier_id = models.ForeignKey(Provedor, on_delete=models.CASCADE,null=False,verbose_name='Provedor' ,)
+    def __str__(self):
+             return self.item_name
+   
 #posicion
-class positions(models.Model):
-    position_id = models.AutoField(primary_key=True, null=False)    
+class posicione(models.Model):
+    position_id = models.AutoField(primary_key=True, verbose_name='ID', null=False)    
     posotion_index = models.PositiveIntegerField(null=False)
     position_aviable = models.CharField(max_length=50, null=False)
-
+    def __str__(self):
+        return str(self.position_id )
 #stock
 class stock(models.Model):
-    stock_id = models.AutoField(primary_key=True, null=False)
-    stock_item_id = models.ForeignKey(items, on_delete=models.CASCADE,null=False)
-    stock_position_id = models.ForeignKey(positions, on_delete=models.CASCADE,null=False)
-    stock_deseado = models.SmallIntegerField(null=False)
+    stock_id = models.AutoField(primary_key=True, verbose_name='ID', null=False,)
+    stock_item_id = models.ForeignKey(productos, on_delete=models.CASCADE,null=False,verbose_name='producto',)
+    stock_position_id = models.ForeignKey(posicione, on_delete=models.CASCADE,null=False,verbose_name='posicion',)
+    
+    stock_itme_amount = models.IntegerField(verbose_name='Cantidad',default=0)
+    stock_deseado = models.SmallIntegerField(null=False,verbose_name='Nesesarios',)
+    def __str__(self):
+        return str(self.stock_item_id)
 
 #usuarios
-class users(models.Model):
-    user_id = models.AutoField(verbose_name='nombre',primary_key=True, null=False)
-    user_password =models.CharField(max_length=8, null=False)
-    user_name =models.CharField(max_length=100, null=False)
-    user_availability=models.BooleanField(null=False)
+class usuarios(models.Model):
+    user_id = models.AutoField( verbose_name='ID', primary_key=True, null=False)
+    user_name =models.CharField(max_length=100, null=False,verbose_name='Nombre',)
+    user_password =models.CharField(max_length=8, null=False,verbose_name='Contraseña',)
+    user_availability=models.BooleanField(null=False,verbose_name='Activo',)
+    def __str__(self):
+             return self.user_name
 
 #stock tracking
 class stock_tracking(models.Model):
-    st_id = models.AutoField(primary_key=True, null=False)
-    st_id_stock =models.ForeignKey(stock, on_delete=models.CASCADE,null=False)
-    st_id_user =models.ForeignKey(users, on_delete=models.CASCADE,null=False)
-    st_prev=models.IntegerField(null=True)
-    st_act=models.IntegerField(null=True)
+    st_id = models.AutoField(primary_key=True, verbose_name='ID',null=False)
+    st_id_stock =models.ForeignKey(stock, on_delete=models.CASCADE,null=False,verbose_name='Stock del Producto',)
+    st_id_user =models.ForeignKey(usuarios, on_delete=models.CASCADE,null=False,verbose_name='Quien hace el movimiento',)
+    st_prev=models.IntegerField(verbose_name='Stock anterior')
+    st_act=models.IntegerField(verbose_name='Stock nuevo')
+    def __str__(self):
+        return str(self.st_id)
+    
 
 
