9403ea2caa0d5141876170dee3f542a3635d67ef
amponio17 <tonylvhdp2@gmail.com>
2025-04-30 00:49:38 -0600

    no me acuerdo

--- invetario/settings.py
+++ invetario/settings.py
@@ -86,18 +86,14 @@ WSGI_APPLICATION = 'invetario.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
     
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'django_inventario',
-        'USER':'root',
-        'PASSWORD':'Lopez0906lv',
-        'HOST':'localhost',
-        'PORT':'3307',
-
-    }
 }
-
+ 
 
 
 # Password validation
