85028e7235f0c5015c28389b57dc965279891ac4
amponio17 <tonylvhdp2@gmail.com>
2025-04-29 22:40:08 -0600

    descomente el comando

--- invetario/settings.py
+++ invetario/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from .db import MYSQL
 import dj_database_url 
-
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
